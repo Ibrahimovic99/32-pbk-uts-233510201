8d9d8a52c3c70ef874099d70953839707a42caf0
Ibrahimovic99 <zlatanibrahimovic@student.uir.ac.id>
2025-05-07 22:56:12 +0700

    filter tugas

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@ const tugas = ref([
   { id: 2, text: 'minum', status: true },
   { id: 3, text: 'joging', status: false },
 ])
-
 const input = ref('')
+const filterr = ref('all')
 
 const jumlahTugas = computed(() => {
   return tugas.value.filter((tugas) => tugas.status === false).length
@@ -28,7 +28,15 @@ const hapusTugas = (id) => {
   tugas.value = tugas.value.filter((tugas) => tugas.id !== id)
 }
 
-
+const filterTugas = computed(() => {
+  if (filterr.value === 'all') {
+    return tugas.value
+  } else if (filterr.value === 'active') {
+    return tugas.value.filter((tugas) => tugas.status === false)
+  } else if (filterr.value === 'completed') {
+    return tugas.value.filter((tugas) => tugas.status === true)
+  }
+})
 
 </script>
 
@@ -37,8 +45,13 @@ const hapusTugas = (id) => {
     <h1>Tugas</h1>
     <input type="text" @keyup.enter="tambahTugas" v-model="input" placeholder="Tambah tugas">
     <button @click="tambahTugas">Tambah</button>
+    <select v-model="filterr">
+      <option value="all">Semua</option>
+      <option value="active">Aktif</option>
+      <option value="completed">Selesai</option>
+    </select>
     <ul>
-      <li v-for="tugas in tugas" :key="tugas.id">
+      <li v-for="tugas in filterTugas" :key="tugas.id">
         <input type="checkbox" v-model="tugas.status">
         {{ tugas.text }} - {{ tugas.status }}
         <button @click="hapusTugas(tugas.id)">Hapus</button>
