b318980967734e5c05fbe5e35e1e6c03dcb18ac2
Ibrahimovic99 <zlatanibrahimovic@student.uir.ac.id>
2025-05-07 22:42:37 +0700

    menampilkan tugas

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue'
+
+const tugas = ref([
+  { id: 1, text: 'makan', status: 'Belum Selesai' },
+  { id: 2, text: 'minum', status: 'Selesai' },
+  { id: 3, text: 'joging', status: 'Belum Selesai' },
+])
+</script>
+
+<template>
+  <div>
+    <h1>Tugas</h1>
+    <ul>
+      <li v-for="tugas in tugas" :key="tugas.id">
+        {{ tugas.text }} - {{ tugas.status }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
