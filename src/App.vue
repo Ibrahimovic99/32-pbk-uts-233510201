00abc4c3a7cb9ab46ff6d370f720153e78c9a85b
Ibrahimovic99 <zlatanibrahimovic@student.uir.ac.id>
2025-05-07 22:47:02 +0700

    input tugas baru

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,26 @@ const tugas = ref([
   { id: 2, text: 'minum', status: 'Selesai' },
   { id: 3, text: 'joging', status: 'Belum Selesai' },
 ])
+
+const input = ref('')
+
+function tambahTugas() {
+  if (input.value !== '') {
+    tugas.value.push({
+      id: tugas.value.length + 1,
+      text: input.value,
+      status: 'Belum Selesai'
+    })
+    input.value = ''
+  }
+}
 </script>
 
 <template>
   <div>
     <h1>Tugas</h1>
+    <input type="text" @keyup.enter="tambahTugas" v-model="input" placeholder="Tambah tugas">
+    <button @click="tambahTugas">Tambah</button>
     <ul>
       <li v-for="tugas in tugas" :key="tugas.id">
         {{ tugas.text }} - {{ tugas.status }}
@@ -19,6 +34,4 @@ const tugas = ref([
   </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
